6c6bf47c0035ed8b8a0742b569b3f1ff0b1ba285
ShivaGanji88 <shivaganji77@gmail.com>
2023-08-29 21:39:22 +0530

    project 1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-a = 1
-b = 2
-print(a==b)
+print("Welcome to band name generator")
+city = input("what's the name of the city you grew up in \n")
+street = input("What's your pet's name \n")
+print("Your brand could be " + city + " " + street)
